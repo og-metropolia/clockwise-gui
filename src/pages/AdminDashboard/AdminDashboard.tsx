1a52587645033b02210b38dbdb7f868969e515cd
gashiAdrian <gashiad@outlook.com>
2024-03-14 15:50:17 +0200

    Co-authored-by: 4MBL <4mbl@users.noreply.github.com> Co-authored-by: yamirh <yamirh@users.noreply.github.com>

--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,91 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from './AdminDashboard.module.css';
-import ProfileCard from '@/components/ProfileCard';
-import ROUTES from '@/constants/routes';
 import { fetchGraphql } from '@/graphql/fetch';
-import { getCompanies, getUsersByCompany } from '@/graphql/queries';
-import { useUser } from '@/components/UserContext';
-import { LoginUser } from '@/types/user';
-import Footer from '@/components/Footer';
+import { getCompanies } from '@/graphql/queries';
 import { List, ListItem, Box, Button, Typography } from '@mui/material';
 import { Company } from '@/types/company';
 
-// const AdminDashboard = () => {
-//   const navigate = useNavigate();
-//   const { getUser } = useUser();
-//   const user = getUser();
-
-//   const [employees, setEmployees] = useState<LoginUser[]>([]);
-
-//   useEffect(() => {
-//     fetchGraphql(getEmployeesByCompany, {
-//       companyId: user?.company?.id,
-//       role: ['EMPLOYEE', 'MANAGER'],
-//     }).then((data: any) => {
-//       const employees = (data?.usersByCompany ?? []) as LoginUser[];
-//       setEmployees(employees);
-//     });
-//   }, [user?.company?.id]);
-
-//   const viewEmployeeReport = (employeeId: string) => {
-//     navigate(ROUTES.visit + `/${employeeId}`);
-//   };
-
-//   return (
-//     <div className={styles.basePage}>
-//       <h1 className={styles.title}>Management</h1>
-//       <div className={styles.companyListContainer}>
-//         <List className={styles.companyList}>
-//           {employees.map((employee) => (
-//             <ListItem
-//               key={employee.id}
-//               className={styles.companyListItem}
-//               onClick={() => viewEmployeeReport(employee.id)}
-//             >
-//               <Box
-//                 display="flex"
-//                 flexDirection="row"
-//                 alignItems="center"
-//                 justifyContent="space-between"
-//                 width="100%"
-//               >
-//                 <ProfileCard user={employee} />
-//               </Box>
-//             </ListItem>
-//           ))}
-//         </List>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default AdminDashboard;
-
-{
-  /* <Box>
-{companies.map((company) => (
-  <Box key={company.id} mb={2}>
-    <Typography variant="h6">{company.name}</Typography>
-    <List>
-      {company.managers.map((manager) => (
-        <ListItem key={manager.id}>
-          <Typography variant="subtitle1">{manager.name} - Manager</Typography>
-          <List>
-            {manager.employees.map((employee) => (
-              <ListItem key={employee.id}>
-                <Typography variant="body2">{employee.name} - {employee.position}</Typography>
-              </ListItem>
-            ))}
-          </List>
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-))}
-</Box> */
-}
 
 const AdminDashboard = () => {
   const [companies, setCompanies] = useState<Company[]>([]);
